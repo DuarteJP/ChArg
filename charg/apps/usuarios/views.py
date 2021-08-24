196f587893b6dd1e0807b1e9d5747a3deec5eff9
Santiago <santiagocarlisi@gmail.com>
2021-08-23 23:09:23 -0300

    set up urls y views para usuarios

--- charg/apps/usuarios/views.py
+++ charg/apps/usuarios/views.py
@@ -1,3 +1,18 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def listar_usuarios(request):
+    return HttpResponse("listado de usuarios")
+
+def crear_usuario(request):
+    return HttpResponse("crear usuario")
+
+def ver_usuario(request, id):
+    return HttpResponse(f"ver usuario {id}")
+
+def modificar_usuario(request, id):
+    return HttpResponse(f"modificar usuario {id}")
+
+def eliminar_usuario(request, id):
+    return HttpResponse(f"eliminar usuario {id}")
+
--- charg/charg/urls.py
+++ charg/charg/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth
 from .import views
+from apps.usuarios.views import listar_usuarios,crear_usuario,eliminar_usuario,modificar_usuario,ver_usuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,13 @@ urlpatterns = [
     path('logout/',auth.LogoutView.as_view(), name='logout'),
 
     #path direccion a las app
-    #path('productos/', include('apps.productos.urls'))    
+    #path('productos/', include('apps.productos.urls'))  
+    
+    #usuarios
+    path('usuarios/listado', listar_usuarios, name='listar_usuarios'),
+    path('usuarios/crear', crear_usuario, name='crear_usuario'),
+    path('usuarios/ver/<int:id>', ver_usuario, name='ver_usuario'),
+    path('usuarios/modificar/<int:id>', modificar_usuario, name='modificar_usuario'),
+    path('usuarios/eliminar/<int:id>', eliminar_usuario, name='eliminar_usuario')
+    
 ]
