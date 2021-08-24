5d3672c72d0e4512acd5b154d970c010d4e5511a
Rodrigo Cattarozzi <rodrigoecattarozzi@gmail.com>
2021-08-24 15:08:10 -0300

    .

--- charg/charg/settings/local.py
+++ charg/charg/settings/local.py
@@ -8,9 +8,9 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'BDCHARG',
+        'NAME': 'FINAL',
         'Trusted_Connection':'yes',
-        'HOST':'localhost\\SQLEXPRESS01',
+        'HOST':'localhost\\SQLEXPRESS',
         'OPTIONS':{
             'driver':'SQL Server Native Client 11.0',
         }
