3ef3f8873195902179e3c2a47b164598b5a6cc38
Mauro Cattarozzi <cattarozzi.mauro@gmail.com>
2021-08-25 17:31:47 -0300

    .

--- charg/apps/respuestas/models.py
+++ charg/apps/respuestas/models.py
@@ -6,7 +6,7 @@ class Respuesta(TimeModels):
 	pregunta = models.ForeignKey(Pregunta, related_name = 'mi_pregunta', on_delete = models.CASCADE)
 	descripcion = models.CharField(max_length = 50)
 	correcta = models.BooleanField(default = False)
-	dificultad = models.IntegerField(default = 1)
+	mostrada = models.BooleanField(default = False)
 
 	def __str__(self):
 		return self.descripcion
