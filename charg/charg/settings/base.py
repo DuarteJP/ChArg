1852ab8c3705799e6d2022ef9afbf533644804fb
Rodrigo Cattarozzi <rodrigoecattarozzi@gmail.com>
2021-08-27 14:53:39 -0300

    Update base.py

--- charg/charg/settings/base.py
+++ charg/charg/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
+    #'django_filters',
     'apps.usuarios',
     'apps.categorias',
     'apps.preguntas',
