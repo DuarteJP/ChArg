1b232bfb403eca585bba2e895eff72fd69502804
MarinaArrieta <arrietamarina12@gmail.com>
2021-09-03 13:56:22 -0300

    fix

--- charg/charg/settings/production.py
+++ charg/charg/settings/production.py
@@ -1,8 +1,9 @@
 from ChArg.charg.charg.settings.local import DEBUG
+
 from .base import *
 if ENVIROMENT == "PROD":
     DEBUG = False
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
     if ambiente == "PROD":
         import django_heroku
